cee0f0b0d0f40b23a3135f387fb5df3f915f5c14
t. starynski <toristarynski@gmail.com>
2025-09-28 21:17:30 -0400

    enemy spawner created and works!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,28 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int life = 5;
+    [SerializeField] private int life = 5; //my wife and four beautiful children
+    [SerializeField] private int money = 1000; //<3 government stipend
+    //surely nothing bad will happen to my family
+    
+    public List<WaveSpawner> waves;
+    public static GameManager instance;
+    public UnityEvent onChanged;
     public UnityEvent onDeath;
 
     void Awake()
     {
+        //no duplicate game managers!
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicated WavesManager, ignoring this one", gameObject);
+        }
+
+        //gets a reference to teh endpoint detector and listens out for the onReachEnd event
         var endPointDetector = GameObject.Find("EndPoint").GetComponent<EndPointDetection>();
         endPointDetector.onReachEnd.AddListener(loseLife);
     }
@@ -30,6 +47,16 @@ public class GameManager : MonoBehaviour
     public void loseLife()
     {
         life--;
+    }
+
+    public int getMoney()
+    {
+        return money;
+    }
+    //pass positive m to gain money, pass negative m to spend money
+    public void changeMoney(int m)
+    {
+        money += m;
     }
 
     void OnDestroy()
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float startTime = 5;
+    [SerializeField] private float endTime = 10;
+    [SerializeField] private float spawnRate = 2;
+
+    void Start()
+    {
+        GameManager.instance.waves.Add(this);
+        InvokeRepeating("Spawn", startTime, spawnRate);
+        Invoke("EndSpawner", endTime);
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+
+    void EndSpawner()
+    {
+        GameManager.instance.waves.Remove(this);
+        CancelInvoke();
+    }
+}
